d25b52119bbe253aea6f98580930b8afc24f5425
Michael Manring <michael@manring>
2022-07-26 12:52:11 +0700

    refactor test to bench for totient package

--- totient/totient_test.go
+++ totient/totient_test.go
@@ -7,19 +7,21 @@ package totient
 
 import "testing"
 
-func TestIsPerfect(t *testing.T) {
+func BenchmarkIsPerfect(b *testing.B) {
 	//perfect := []uint{3, 9, 15, 27, 39, 81, 111, 183, 243, 255, 327, 363, 471, 729, 2187, 2199, 3063, 4359, 4375, 5571}
 	perfect := []uint{3, 9, 15, 27, 39, 81, 111, 183, 243, 255, 327, 363, 471, 729, 2187, 2199, 3063, 4359, 4375, 5571, 6561, 8751, 15723, 19683, 36759}
 	processor := New(uint(len(perfect)))
-	for i := 1; i < len(perfect); i += 2 {
-		for _, j := range perfect[i-1 : i+1] {
-			if processor.IsPerfect(j) == false {
-				t.Errorf("incorrect result: expected true, got false")
+	for k := 0; k < b.N; k++ {
+		for i := 1; i < len(perfect); i += 2 {
+			for _, j := range perfect[i-1 : i+1] {
+				if processor.IsPerfect(j) == false {
+					b.Errorf("incorrect result: expected true, got false")
+				}
 			}
-		}
-		for j := perfect[i-1] + 1; j < perfect[i]; j++ {
-			if processor.IsPerfect(j) == true {
-				t.Errorf("incorrect result: expected false, got true")
+			for j := perfect[i-1] + 1; j < perfect[i]; j++ {
+				if processor.IsPerfect(j) == true {
+					b.Errorf("incorrect result: expected false, got true")
+				}
 			}
 		}
 	}
