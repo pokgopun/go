a482a197677ccda10c2be4874ae96bfaf4e8e75d
Michael Manring <michael@manring>
2022-07-30 00:26:35 +0700

    simplify api for totient package

--- totient/totient.go
+++ totient/totient.go
@@ -12,72 +12,203 @@ Output
 package totient
 
 import (
-	"math/big"
+	"github.com/jbarham/primegen"
 )
 
+// processor allow totient computation to use cache which improve performance
 type processor struct {
-	m map[uint]uint
-	//	one, min, max *big.Int
-	//mot uint
-	//fp  map[uint]bool
+	m map[uint64]uint64 // cache totient value to avoid unnecessary computation
+	//pg *primegen.Primegen // being used by Factor() when Euler's product formula is used to compute Totient()
 }
 
-func New(n uint) processor {
+// New() return a pointer of processor
+func New() *processor {
+	// intialize *processor and allow lenghty manipulation of its fields
 	/*
-		fp := make(map[uint]bool)
-		for _, v := range fastperfect(n) {
-			fp[v] = true
-		}
+		var p processor
+		p.m = make(map[uint]uint)
+		p.pg = primegen.New()
+		return &p
 	*/
-	//return processor{m: make(map[uint]uint), one: big.NewInt(1), mot: 1, fp: fp}
-	//return processor{m: make(map[uint]uint), mot: 1, fp: fp}
-	return processor{m: make(map[uint]uint)}
+	return &processor{m: make(map[uint64]uint64)}
+	//return &processor{m: make(map[uint64]uint64), pg: primegen.New()}
 }
 
-func (p *processor) Totient(n uint) (r uint) {
-	if n == 1 {
+func (p *processor) Totient(n uint64) (r uint64) {
+	// https://mathworld.wolfram.com/TotientFunction.html
+	// totient(1) = 1, also handle totient(0) at the same time as it can be 0 or 1
+	if n <= 1 {
 		return 1
 	}
 	/**/
-	r, ok := p.m[n]
-	if ok {
-		return r
+	// cache only when n is even as perfect totient check will result in n equal even (i.e. n >= 3, totient(n) is always even)
+	if n%2 == 0 {
+		r, ok := p.m[n]
+		if ok {
+			return r
+		}
 	}
 	/**/
+	/*
+		// calcuation using Euler's product formula, need factorization which adds lots of overhead if not probability based implementation
+				r = n
+				var seen uint64
+				for _, v := range p.Factor(n) {
+					if seen == v {
+						continue
+					}
+					seen = v
+					r /= v
+					r *= v - 1
+				}
+	*/
+	/**/
+	// calcuation using GCD
 	r = 1 // 1 is relatively prime to any number
-	//p.max = big.NewInt(int64(n))
-	//for p.min = big.NewInt(2); p.min.Cmp(p.max) == -1; p.min.Add(p.min, p.one) {
-	//if new(big.Int).GCD(nil, nil, p.min, p.max).Cmp(p.one) == 0 {
-	for i := uint(2); i < n; i++ {
+	for i := uint64(2); i < n; i++ {
 		if GCDEuclidean(i, n) == 1 {
 			r++
 		}
 	}
-	p.m[n] = r
+	/**/
+	// only cache when n is even as it can be reused by another IsPerfect perform iteration of totient (i.e. n >= 3, totient(n) is always even)
+	/**/
+	if n%2 == 0 {
+		p.m[n] = r
+	}
+	/**/
 	return r
 }
 
-func (p *processor) IsPerfect(n uint) bool {
-	if n < 2 || n%2 == 0 || n > 3 && big.NewInt(int64(n)).ProbablyPrime(0) {
+// IsPerfect() take an uint64  and check if it is a perfect totient and return bool
+func (p *processor) IsPerfect(n uint64) bool {
+	// a perfect totient is always an odd number start at 3
+	if n < 2 || n%2 == 0 {
 		return false
 	}
-	// check for fastperfect
-	/*
-		if p.fp[n] {
-			return true
-		}
-	*/
-	var sum, tmp uint = 1, p.Totient(n)
-	for tmp > 1 {
-		sum += tmp
+	// initial value of sum is totient(1)=1 which is where we stop the iteration
+	var sum uint64 = 1
+	// initial value for totient(n) for further iteration
+	var t uint64 = p.Totient(n)
+	for t > 1 {
+		sum += t
 		if sum > n {
 			return false
 		}
-		tmp = p.Totient(tmp)
+		t = p.Totient(t)
 	}
 	return sum == n
 }
-func fastperfect(n uint) (r []uint) {
+func (p *processor) ListPerfect(start uint64, count uint64) (s []uint64) {
+	// save and restore the current state of pg if reusing an existing one that may be being used by other method at the same time (i.e. Factor)
+	/*
+		prev := p.pg.Peek()
+		defer func() {
+			p.pg.SkipTo(prev)
+		}()
+		pg := p.pg
+	*/
+	/**/
+	pg := primegen.New()
+	pc := newPerfectCandidate(start, pg)
+	// iterate, check and store the candidate that is a perfect totient
+	var c uint64
+	for count > 0 {
+		c = pc.next()
+		if p.IsPerfect(c) {
+			s = append(s, c)
+			count--
+		}
+	}
+	/**/
+	return s
+}
+
+// perfect totient candidate generator to avoid processing even and prime numbers (i.e. except for 3)
+type perfectCandidate struct {
+	val uint64
+	pg  *primegen.Primegen
+}
+
+// newPerfectCandidate() takes an uint64 and primegen and then return a pointer of perfectCandidate that use the number to set initial candidate
+func newPerfectCandidate(start uint64, pg *primegen.Primegen) *perfectCandidate {
+	// 1st perfect totient candidate is 3 and the rest are non-prime odd
+	if start < 3 {
+		start = 3
+	} else if start%2 == 0 {
+		start++
+	}
+	// an initial prime that a perfect totient will never be is the prime number next to the start value
+	pg.SkipTo(start)
+	for pg.Peek() <= start {
+		pg.Next()
+	}
+	return &perfectCandidate{val: start, pg: pg}
+}
+
+// next() return the current candidate and advance the next one
+func (pc *perfectCandidate) next() uint64 {
+	// advance the odd if it is the current prime, advance the prime if it has been checked against the odd
+	for pc.pg.Peek() <= pc.val {
+		if pc.val == pc.pg.Peek() {
+			pc.val += 2
+		}
+		pc.pg.Next()
+	}
+	// if it the current odd is not the current prime and then advance the odd and return the old one
+	pc.val += 2
+	return pc.val - 2
+}
+
+/*
+func (p *processor) Factor(n uint64) (s []uint64) {
+	switch n {
+	case 0, 1:
+		return []uint64{}
+	case 2, 3:
+		return []uint64{n}
+	}
+	lim := uint64(math.Sqrt(float64(n)))
+	prev := p.pg.Peek()
+	defer func() {
+		p.pg.SkipTo(prev)
+	}()
+	p.pg.Reset()
+	d := p.pg.Next()
+	for {
+		if n%d == 0 {
+			s = append(s, d)
+			n /= d
+			if n == 1 {
+				break
+			}
+			continue
+		}
+		d = p.pg.Next()
+		if d > lim {
+			break
+		}
+	}
+	if n > 1 {
+		s = append(s, n)
+	}
+	return s
+}
+*/
+func GCDEuclidean(a, b uint64) uint64 {
+	for a != b {
+		if a > b {
+			a -= b
+		} else {
+			b -= a
+		}
+	}
+
+	return a
+}
+
+/*
+func FastPerfect(n uint) (r []uint) {
 	var sf []uint
 	var mot uint = 3
 	for _, v := range []uint{5, 17, 257, 65537} {
@@ -103,18 +234,15 @@ func fastperfect(n uint) (r []uint) {
 			s = append(s, j*3)
 		}
 		mot *= 3
-		//if len(s) > 0 && s[0] < mot {
 		for len(s) > 0 && s[0] < mot {
-			//fmt.Println(s[0])
 			r = append(r, s[0])
 			s = s[1:]
 			n--
 			if n == 0 {
-				break
+				goto end
 			}
 		}
 		if len(sf) > 0 && sf[0] < mot {
-			//fmt.Println(sf[0])
 			r = append(r, sf[0])
 			sf = sf[1:]
 			n--
@@ -122,23 +250,13 @@ func fastperfect(n uint) (r []uint) {
 				break
 			}
 		}
-		//fmt.Println(mot)
 		r = append(r, mot)
 		n--
 		if n == 0 {
 			break
 		}
 	}
+end:
 	return r
 }
-func GCDEuclidean(a, b uint) uint {
-	for a != b {
-		if a > b {
-			a -= b
-		} else {
-			b -= a
-		}
-	}
-
-	return a
-}
+*/
--- totient/totient_test.go
+++ totient/totient_test.go
@@ -5,23 +5,69 @@ Write a script to print first 20 Gapful Numbers greater than or equal to 100. Pl
 */
 package totient
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
 
 func BenchmarkIsPerfect(b *testing.B) {
-	//perfect := []uint{3, 9, 15, 27, 39, 81, 111, 183, 243, 255, 327, 363, 471, 729, 2187, 2199, 3063, 4359, 4375, 5571}
-	perfect := []uint{3, 9, 15, 27, 39, 81, 111, 183, 243, 255, 327, 363, 471, 729, 2187, 2199, 3063, 4359, 4375, 5571, 6561, 8751, 15723, 19683, 36759}
-	processor := New(uint(len(perfect)))
+	perfect := []uint64{3, 9, 15, 27, 39, 81, 111, 183, 243, 255, 327, 363, 471, 729, 2187, 2199, 3063, 4359, 4375, 5571}
+	//perfect := []uint64{3, 9, 15, 27, 39, 81, 111, 183, 243, 255, 327, 363, 471, 729, 2187, 2199, 3063, 4359, 4375, 5571, 6561, 8751, 15723, 19683, 36759, 46791}
+	l := len(perfect)
+	processor := New()
 	for k := 0; k < b.N; k++ {
-		for i := 1; i < len(perfect); i += 2 {
-			for _, j := range perfect[i-1 : i+1] {
-				if processor.IsPerfect(j) == false {
-					b.Errorf("incorrect result: expected true, got false")
-				}
-			}
+		i := 0
+		if processor.IsPerfect(perfect[i]) == false {
+			b.Errorf("IsPerfect(%d): incorrect result: expected true, got false", perfect[i])
+		}
+		for i = 1; i < l; i += 1 {
 			for j := perfect[i-1] + 1; j < perfect[i]; j++ {
 				if processor.IsPerfect(j) == true {
-					b.Errorf("incorrect result: expected false, got true")
+					b.Errorf("perfect(%d): incorrect result: expected false, got true", j)
 				}
+			}
+			if processor.IsPerfect(perfect[i]) == false {
+				b.Errorf("IsPerfect(%d): incorrect result: expected true, got false", perfect[i])
+			}
+		}
+	}
+}
+
+func BenchmarkListPerfect(b *testing.B) {
+	data := []struct {
+		start, count uint64
+		res          []uint64
+	}{
+		/**/
+		{
+			3, 20,
+			[]uint64{
+				3, 9, 15, 27, 39,
+				81, 111, 183, 243, 255,
+				327, 363, 471, 729, 2187,
+				2199, 3063, 4359, 4375, 5571,
+			},
+		},
+		/**/
+		/*
+			{
+				3, 26,
+				[]uint64{
+					3, 9, 15, 27, 39,
+					81, 111, 183, 243, 255,
+					327, 363, 471, 729, 2187,
+					2199, 3063, 4359, 4375, 5571,
+					6561, 8751, 15723, 19683, 36759, 46791,
+				},
+			},
+		*/
+	}
+	processor := New()
+	for k := 0; k < b.N; k++ {
+		for _, d := range data {
+			if diff := cmp.Diff(d.res, processor.ListPerfect(d.start, d.count)); diff != "" {
+				b.Error(diff)
 			}
 		}
 	}
